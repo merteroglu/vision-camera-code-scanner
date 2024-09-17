07881f4f8994a5883626e6c22463c0b3f428ce6c
Mert Eroğlu <merteroglu16@gmail.com>
2024-09-17 09:44:24 +0300

    fix xcode v15

---
+++ ios/VisionCameraCodeScanner.h
@@ -0,0 +1,8 @@
+#import <VisionCamera/FrameProcessorPlugin.h>
+#ifndef VisionCameraCodeScanner_h
+#define VisionCameraCodeScanner_h
+
+#endif
+
+@interface VisionCameraCodeScanner : NSObject<FrameProcessorPluginBase>
+@end
